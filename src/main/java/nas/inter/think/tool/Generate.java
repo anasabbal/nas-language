e07fcc182c919f78711c0a1921a8ec227b3b6ab4
Anas Ouzoline <87352192+anasabbal@users.noreply.github.com>
2022-10-06 16:57:04 +0100

    #101 contains the code generated by this script once we’ve finished implementing nas and defined all of its syntax tree nodes

--- src/main/java/nas/inter/think/tool/Generate.java
+++ src/main/java/nas/inter/think/tool/Generate.java
@@ -2,6 +2,8 @@ package nas.inter.think.tool;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 import java.util.List;
 
 public class Generate {
@@ -12,24 +14,57 @@ public class Generate {
             System.exit(64);
         }
         String outputDir = args[0];
+        defineAst(outputDir, Arrays.asList(
+                "Binary   : Expr left, Token operator, Expr right",
+                "Grouping : Expr expression",
+                "Literal  : Object value",
+                "Unary    : Token operator, Expr right"
+        ));
     }
-    private static void defineAst(String outputDir, String baseName, List<String> types){
-        String path = outputDir + "/" + baseName + ".java";
+    // defineAst needs to do is output the base Expr class
+    private static void defineAst(String outputDir, List<String> types){
+        String path = outputDir + "/" + "Expression" + ".java";
 
         try {
-            PrintWriter writer = new PrintWriter(path, "UTF-8");
+            PrintWriter writer = new PrintWriter(path, StandardCharsets.UTF_8);
 
             writer.println("package nas.inter.think.nas;");
             writer.println();
             writer.println("import java.util.List;");
             writer.println();
-            writer.println("abstract class " + baseName + " {");
+            writer.println("abstract class " + "Expression" + " {");
 
+            // The AST classes.
+            for(String type: types){
+                String className = type.split(":")[0].trim();
+                String fields = type.split(":")[1].trim();
+                defineType(writer, className, fields);
+            }
             writer.println("}");
             writer.close();
         }catch (IOException e){
             e.printStackTrace();
         }
+    }
+    // It declares each field in the class body. It defines a constructor for the class with parameters for each field and initializes them in the body
+    private static void defineType(PrintWriter writer, String className, String fields) {
+        writer.println("  static class " + className + " extends " +
+                "Expression" + " {");
+        // Constructor
+        writer.println("    " + className + "(" + fields + ") {");
+        // Store parameters in fields.
+        String[] _fields = fields.split(", ");
+        for (String field : _fields) {
+            String name = field.split(" ")[1];
+            writer.println("      this." + name + " = " + name + ";");
+        }
+        writer.println("    }");
+        // Fields.
+        writer.println();
+        for (String field : _fields) {
+            writer.println("    final " + field + ";");
+        }
 
+        writer.println("  }");
     }
 }
